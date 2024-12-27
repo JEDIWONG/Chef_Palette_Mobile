471f46c2cb2bfc8e93f5b3f52fb6aca21ff2dfaf
JEDIWONG <jediwzk@gmail.com>
2024-12-27 14:05:21 +0800

    cart and checkout

--- lib/screen/checkout.dart
+++ lib/screen/checkout.dart
@@ -16,6 +16,7 @@ class Checkout extends StatelessWidget {
         leadingWidth: MediaQuery.sizeOf(context).width * 0.3,
         bottom: AppBar(
           automaticallyImplyLeading: false,
+          centerTitle: true,
           title: Text(
             "Review and Pay",
             style: CustomStyle.h1,
@@ -34,6 +35,13 @@ class Checkout extends StatelessWidget {
               endIndent: 30,
             ),
             OrderSummary(),
+
+            Divider(
+              color: Colors.grey,
+              thickness: 1,
+              indent: 20,
+              endIndent: 30,
+            ),
           ],
         ),
       ),
@@ -50,6 +58,34 @@ class OrderSummary extends StatelessWidget {
     return await firestoreService.getCartItems();
   }
 
+  Future <double> calculateTotalPrice() async {
+    
+    List<CartItemModel> cartItems = await fetchCartItems();
+
+    double totalPrice = 0.0;
+    
+    for (CartItemModel item in cartItems) {
+      totalPrice += item.price * item.quantity; 
+    }
+
+    return totalPrice.toDouble();
+  }
+
+  Future <double> calculateTax() async {
+    
+    List<CartItemModel> cartItems = await fetchCartItems();
+
+    double totalPrice = 0.0;
+    
+    for (CartItemModel item in cartItems) {
+      totalPrice += item.price * item.quantity; 
+    }
+
+    return totalPrice.toDouble();
+  }
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -58,29 +94,50 @@ class OrderSummary extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("Order Summary", style: CustomStyle.h2),
-          FutureBuilder<List<CartItemModel>>(
-            future: fetchCartItems(), // Fetch cart items here
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text("Error: ${snapshot.error}"));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text("Your cart is empty"));
-              } else {
-                return Column(
-                  children: snapshot.data!.map((cartItem) {
-                    return OrderItem(
-                      name: cartItem.name,
-                      quantity: cartItem.quantity,
-                      price: cartItem.price,
-                      
-                    );
-                  }).toList(),
-                );
-              }
-            },
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: FutureBuilder<List<CartItemModel>>(
+              future: fetchCartItems(), // Fetch cart items here
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text("Error: ${snapshot.error}"));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text("Your cart is empty"));
+                } else {
+                  return Column(
+                    children: snapshot.data!.map((cartItem) {
+                      return OrderItem(
+                        name: cartItem.name,
+                        quantity: cartItem.quantity,
+                        price: cartItem.price,
+                        
+                      );
+                    }).toList(),
+                  );
+                }
+              },
+            ),
           ),
+
+          ListTile(
+            leading: Text("Subtotal : ", style: CustomStyle.txt),
+            trailing: FutureBuilder<double>(
+              future: calculateTotalPrice(), // Call the asynchronous function
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                } else if (snapshot.hasData) {
+                  return Text("RM ${snapshot.data!.toStringAsFixed(2)}"); // Display total price
+                } else {
+                  return const Text("RM 0.00");
+                }
+              },
+            ),
+          )
+
         ],
       ),
     );
