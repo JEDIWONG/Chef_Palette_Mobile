f6ea97e5e6b14be75ab9b0ab6b2c7124a84cd6bd
leecp02 <chunpan08097@gmail.com>
2025-01-09 16:22:18 +0800

    admin user part

--- lib/admin/admin_user.dart
+++ lib/admin/admin_user.dart
@@ -1,9 +1,41 @@
 import 'package:chef_palette/admin/component/member_card.dart';
+import 'package:chef_palette/models/user_model.dart';
+import 'package:chef_palette/services/firestore_services.dart';
 import 'package:chef_palette/style/style.dart';
 import 'package:flutter/material.dart';
 
-class AdminUser extends StatelessWidget{
+class AdminUser extends StatefulWidget {
   const AdminUser({super.key});
+
+  @override
+  State<AdminUser> createState() => _AdminUserState();
+}
+
+class _AdminUserState extends State<AdminUser> {
+  final FirestoreService _firestoreService = FirestoreService();
+  List<UserModel> users = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsers();
+  }
+
+  Future<void> _loadUsers() async {
+    try {
+      final fetchedUsers = await _firestoreService.fetchAllUsers();
+      setState(() {
+        users = fetchedUsers;
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print('Error loading users: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +45,32 @@ class AdminUser extends StatelessWidget{
         centerTitle: true,
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
-        
         title: Text(
           "Members",
           style: CustomStyle.h3,
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 50,),
-          const MemberCard(memberId: "12dawwwwwwwwdwdwada", joinDate: "12-12-2024")
-        ],
-      ),
+      body: isLoading 
+        ? const Center(child: CircularProgressIndicator())
+        : users.isEmpty 
+          ? const Center(child: Text('No members found'))
+          : Column(
+              children: [
+                const SizedBox(height: 50),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      final user = users[index];
+                      return MemberCard(
+                        memberId: user.uid,
+                        joinDate: user.joinDate,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
     );
   }
-  
 }
